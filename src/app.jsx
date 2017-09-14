624cd33b0aa6d788508c57f539fafa4c6622912d
Artem Besedin <speker@mail.ru>
2017-09-14 21:44:48 +0400

    Project init

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/components/App';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+    <App/>,
+app);
---
+++ src/app/components/Dev.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Dev extends React.Component
+{
+    render() {
+        return <p>Developer: Speker(speker@mail.ru)</p>;
+    }
+}
